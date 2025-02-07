55c0f3af21a0489374a971ae1a20ae9c0b108c3f
rianadb <rianadbejarin@gmail.com>
2025-02-07 12:41:58 +0800

    modified student model

--- src/session/models.py
+++ src/session/models.py
@@ -8,15 +8,15 @@ class Student(AbstractUser):
     first_name = None
     last_name = None
 
-    # username = models.CharField(max_length=20, unique=True)
+    username = models.CharField(max_length=20)
     email = models.EmailField(_("email address"), unique=True)
 
     agreement = models.BooleanField()
 
-    # USERNAME_FIELD = "email"
-    # REQUIRED_FIELDS = ["username", "password"]
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "password"]
 
     objects = StudentManager()
 
     def __str__(self):
-        return self.username
+        return self.email
