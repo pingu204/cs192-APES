23924793fceddaa5d92bc672c7bc0de7a995a29c
unknown <ghsalao@up.edu.ph>
2025-02-10 23:25:59 +0800

    edit: reconfigured APES database headeer and index title

--- src/session/admin.py
+++ src/session/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from .models import Student
 
+# APES Database re-configuration
+admin.site.site_header = "APES Administration"
+admin.site.index_title = "APES Admin Portal"
+
+
 # Register your models here.
+
+# Affixed Student User view in backend, hence the decorator @admin.register(Student)
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('email', 'username', 'is_staff')
