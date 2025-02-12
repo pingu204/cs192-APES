fed4f27b5a6b9b3a454e565e4673857beddba8fd
TheooooW <theodoreramonarturo@gmail.com>
2025-02-13 02:19:44 +0800

    fix: test_views (FINAL TEST ONG)

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -38,22 +38,22 @@ class TestViewsGuest(TestCase):
     
     def setUp(self):
         self.client = Client()
-        self.login_url = reverse('guest_login_view')
+        self.guest_login_url = reverse('guest_login_view')
     
-    def test_guest_login_POST(self): 
-        response = self.client.post(self.login_url)
+    def test_guest_login(self): 
+        response = self.client.post(self.guest_login_url)
         self.assertEqual(response.status_code, 302)  # Assuming a redirect to homepage
         self.assertRedirects(response, reverse('homepage_view'))
         self.assertTrue(self.client.session.get('is_guest', False))  # Check if 'is_guest' is set to True
 
     def test_logout_POST(self):
         # Log in as guest first
-        self.client.post(self.login_url)
+        self.client.post(self.guest_login_url)
         self.assertTrue(self.client.session.get('is_guest', False))  # Ensure guest login
 
         # Log out
-        logout_url = reverse('logout_view')
-        response = self.client.post(logout_url)
+        guest_logout_url = reverse('logout_view')
+        response = self.client.post(guest_logout_url)
         self.assertEqual(response.status_code, 302)  # Assuming a redirect to landing view
         self.assertRedirects(response, reverse('landing_view'))
         self.assertFalse(self.client.session.get('is_guest', False))  # Ensure 'is_guest' is removed
