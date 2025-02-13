ca92ad4b0474f94abf1811c6ac0d0f0c588e0c23
rianadb <rianadbejarin@gmail.com>
2025-02-13 10:50:26 +0800

    chore: add ImproperlyConfigured to middleware.py

--- src/apes/middleware.py
+++ src/apes/middleware.py
@@ -5,11 +5,11 @@ User-defined middleware for the APES project.
 """
 
 from django.shortcuts import render
-from django.db.utils import OperationalError, DatabaseError
+from django.db.utils import OperationalError, DatabaseError, ImproperlyConfigured
 from django.utils.deprecation import MiddlewareMixin
 
 class DatabaseErrorMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
-        if isinstance(exception, (OperationalError, DatabaseError)):
+        if isinstance(exception, (ImproperlyConfigured, OperationalError, DatabaseError)):
             return render(request, 'database_error.html', {'error': f"Timeout: Database connection failed. Error: {exception}"}, status=500)
         return None
