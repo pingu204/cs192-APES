f612775a3aa2365a729c813e3970849b8444c5c3
pingu204 <73891290+pingu204@users.noreply.github.com>
2025-02-18 22:45:55 +0800

    refactor: scrape data from CRS

--- src/scraper/scrape.py
+++ src/scraper/scrape.py
@@ -9,7 +9,6 @@ import pandas as pd
 def parse_html(url:str, tag:str):
     with requests.get(url) as page:
         soup = BeautifulSoup(page.text, "html.parser")
-
         return soup.find_all(tag)
 
 """ Obtains all possible courses in CRS """
@@ -21,18 +20,23 @@ def get_courses() -> list[dict[str,str]]:
 
     print(f"Scraping from {BASE_URL}...")
     
-    for letter in list(string.ascii_lowercase):
+    # Get set of unique alphabet letters
+    initials = list(string.ascii_lowercase)
+
+    for letter in initials:
+        # Obtain all rows of the table
         soup = parse_html(BASE_URL+letter, "tr")
-        for row in soup[4:]:
+
+        for row in soup[4:]: # Actual entries of the table start at index 4
             tr = row.find_all("td")
 
             # Check if there are no classes
             if (tr[0].text != "No courses to display"):
                 courses.append(
                     {
-                        "course_code" : tr[0].text,
-                        "course_title" : tr[1].text,
-                        "offering_unit" : tr[3].text,
+                        "course_code" :     tr[0].text,
+                        "course_title" :    tr[1].text,
+                        "offering_unit" :   tr[3].text,
                     }
                 )
         print(f"+ '{letter.capitalize()}' courses DONE")
@@ -41,36 +45,44 @@ def get_courses() -> list[dict[str,str]]:
 
 """ Parses course code from `td` element """
 def get_course_code(raw_code: str):
-    # Only extract first line of the element in case of 
-    # multi-line course codes
-    # -- e.g.   Bioinfo 297 HQR<br/>Computational Phylogenetics
-    # --        should be 'Bioinfo 297 HQR'
-    # Remove section names
-    # -- e.g.   Bioinfo 297 HQR should be 'Bioinfo 297'
-    return (' '.join(raw_code.split('\n')[0].split(' ')[:-1])).rstrip()
+    # Only extract first line of the element in case of multi-line course codes
+    # -- e.g. Bioinfo 297 HQR<br/>Computational Phylogenetics should be 'Bioinfo 297 HQR'
+    course_code = raw_code.split('\n')[0]
 
-""" Obtain units of all subjects in CRS from Acad. Years `start` to `end` """
-def get_units(start:int , end:int):
+    # Remove section names
+    # -- e.g. Bioinfo 297 HQR should be 'Bioinfo 297'
+    course_code = ' '.join(course_code.split(' ')[:-1])
+
+    # Remove trailing spaces
+    return course_code.rstrip()
+
+""" Obtain units of all courses in `course_codes` from A.Y. `start` to `end` """
+def get_units(start_year:int, end_year:int, course_codes:list[str]):
+    # Note: start_year must be <= end_year
+    # -- e.g. start_year = 2016, end_year = 2022 scrapes data from A.Y. 2016-2017 to A.Y. 2022-2023
     BASE_URL = "https://crs.upd.edu.ph/schedule/120"
 
     # Container for values
     units: dict[str, float] = {}
+    len_courses = len(course_codes)
 
-    # Load .csv files
-    courses_csv =   pd.read_csv("courses.csv", sep='\t')
-    units_csv =     pd.read_csv("units.csv", sep='\t')
-    
-    # Remove those that already have encoded units
-    course_codes =  list(set(courses_csv["course_code"].tolist()) - set(units_csv["course_code"].tolist()))
+    # Get the last two digits of the years
+    start_year %= 100
+    end_year %= 100
 
-    for year in range(end, start-1, -1): # A.Y 2018-2019 to A.Y. 2024-2025
-        for sem in [1,2]:
-            print(f"A.Y. {year}-{year+1}, Sem {sem}")
+    for year in range(end_year, start_year-1, -1):
+        for sem in [1, 2, 4]: # Semester Number (4 = Midyear)
+            # Marker
+            print(f"\n-- A.Y. {year}-{year+1}, " + (f"Sem {sem}" if sem != 4 else "Midyear") + " --")
             
-            for letter in list(set([word[0] for word in course_codes])):
+            # Get unique initials of remaining course codes
+            initials = list(set([word[0] for word in course_codes]))
+
+            for letter in initials:
+                # Obtain all rows of the table
                 soup = parse_html(f"{BASE_URL}{year}{sem}/{letter}/", "tr")
 
-                for row in soup[1:]:
+                for row in soup[1:]: # Actual entries of the table start at index 1
                     tr = row.find_all("td")
 
                     # Check if there are no classes 
@@ -78,7 +90,7 @@ def get_units(start:int , end:int):
                         course_code = get_course_code(tr[1].get_text(separator='\n'))
                         
                         if course_code not in course_codes:
-                            continue
+                            continue # Units for `course_code` already found; skip to next
                         else:
                             print(f"+ {course_code}")
                             units[course_code] = float(tr[2].text)
@@ -90,16 +102,27 @@ def get_units(start:int , end:int):
         if course_codes == []:
             break
 
-        print(f"Remaining: {len(course_codes)}")
+        
+    print(f"\n[Found: {len_courses - len(course_codes)}/{len_courses}]")
 
-    return pd.DataFrame({"course_code" : list(units.keys()), "units" : list(units.values())})
+    return pd.DataFrame({
+        "course_code" : list(units.keys()), 
+        "units" :       list(units.values()),
+    })
 
-courses_csv = pd.read_csv("courses.csv", sep='\t', index_col=0)
-units_csv = pd.read_csv("units.csv", sep='\t', index_col=0)
+if __name__ == "__main__":
+    course_list = get_courses()
+    print(course_list)
 
-print(courses_csv)
-print(units_csv)
+    units_list = get_units(
+        start_year =    2024,
+        end_year =      2024, 
+        course_codes =  list(set(course_list["course_code"].tolist()))[:10],
+    )
+    print(units_list)
 
-print(courses_csv.merge(units_csv, on="course_code", how="left"))
+    course_list_with_units = course_list.merge(units_list, on="course_code", how="left")
+    print(course_list_with_units)
 
-print(get_courses())
+    course_list_with_units.to_csv("csv/courses.csv", index=False)
+
