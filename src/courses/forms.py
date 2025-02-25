e950b377a7058ecab301cb5c932798d0fb3d3688
rianadb <rianadbejarin@gmail.com>
2025-02-25 18:54:06 +0800

    chore: add error message for UC3-S4

--- src/courses/forms.py
+++ src/courses/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+import os, pandas as pd
 from .models import DesiredCourse
 
 class DesiredClassesForm(forms.Form):
@@ -12,3 +12,23 @@ class DesiredClassesForm(forms.Form):
             }
         )
     )
+
+    # def clean_course_code(self):
+    #     raw_course_code = self.cleaned_data.get("course_code")
+    #     course_code = (' '.join(raw_course_code.split())).upper()
+    #     cwd = os.path.dirname(__file__)
+    
+    #     # Navigate up one directory level
+    #     relative_path = os.path.join(cwd, '..', 'scraper', 'csv', 'courses.csv')
+        
+    #     # Resolve the relative path to an absolute path
+    #     absolute_path = os.path.abspath(relative_path)
+    #     courses = pd.read_csv(absolute_path)
+    #     # print(courses)
+    #     courselist = (courses["course_code"].tolist())
+    #     print("HEY")
+    #     if course_code not in courselist: # UC3-S4 
+    #         raise forms.ValidationError("Class does not exist. Try checking if the entered class code is correct.")
+    #         # print(messages.error(request, "Class does not exist. Try checking if the entered class code is correct."))
+    #         # form.error(request, "Class does not exist. Try checking if the entered class code is correct.")
+    #     return course_code
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from .forms import DesiredClassesForm
-
+from django.contrib import messages
+import pandas as pd
+import os
 
 # Create your views here.
 
 def dcp_add_view(request):
     search_results = []
     form = DesiredClassesForm()
+    # from copilot:
+    # Get the current working directory (CWD)
+    cwd = os.path.dirname(__file__)
+    # Navigate up one directory level
+    relative_path = os.path.join(cwd, '..', 'scraper', 'csv', 'courses.csv')
+    # Resolve the relative path to an absolute path
+    absolute_path = os.path.abspath(relative_path)
+    courses = pd.read_csv(absolute_path)
+    # print(courses)
+    courselist = (courses["course_code"].tolist())
+    # print(courselist)
 
     if request.GET.get("course_code"):
         # get the raw query search placed by the user
@@ -21,7 +34,9 @@ def dcp_add_view(request):
         # Primary Logic (?): if cleaned_search_query == row['course_code'] then append to search_results
 
         # pwede rin check muna if wala, i.e., cleaned_search_query in [course_code1, course_code2, ...]
-
+        if cleaned_search_query.upper() not in courselist and cleaned_search_query.capitalize() not in courselist: # UC3-S4 
+            # print("class does not exist")
+            messages.error(request, "Class does not exist. Try checking if the entered class code is correct.")
         # Now, we use this cleaned_search_query to match a course code! only EXACT MATCHING COURSE CODES
         # i.e. .startswith("course_code") can be used?
         ...
