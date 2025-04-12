1be8f34c88d8004a249aaa9b944b07aa75c1ffe3
pingu204 <73891290+pingu204@users.noreply.github.com>
2025-04-12 20:36:32 +0800

    chore: set `DEBUG = False`

--- src/apes/settings.py
+++ src/apes/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "*",
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
     'courses',
     'preferences',
 
-    'storages',
-    'corsheaders',
+    #'storages',
+    #'corsheaders',
 ]
 
 SESSION_COOKIE_SECURE = False
@@ -106,19 +106,19 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    #'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+""" CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://cs192-apes.onrender.com",
     "http://127.0.0.1"
-]
+] """
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
